09461700c343bc361dd4a46fed27a18c68dc78e1
sandeepkoppolu <sakoppolu@deloitte.com>
2022-10-06 16:53:22 +0000

    Company  name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Deloitte Company';
+    team = 'Deloitte Consulting LLC. ';
     version = '1.0.0';
 }
 module.exports = AppConfig;
